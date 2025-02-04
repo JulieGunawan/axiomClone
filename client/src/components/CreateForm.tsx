82925c5f1f527582952e2a2142e8cd6d2131363f
JulieGunawan <julie.gunawan@protonmail.com>
2025-02-04 15:54:48 -0700

    updated the name

--- client/src/components/CreateForm.tsx
+++ client/src/components/CreateForm.tsx
@@ -25,6 +25,7 @@ const CreateForm = () =>{
         }))
     },500),[]);
 
+    console.log("form Data", formData);
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         const {name, value} = e.target;
         debounceHandleChange(name, value);
@@ -34,6 +35,7 @@ const CreateForm = () =>{
         e.preventDefault();
         try{
             const body = {formData};
+            console.log("body", body);
             await fetch("http://localhost:5000/employees", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
@@ -50,8 +52,8 @@ const CreateForm = () =>{
     return(
         <div className="w-full h-screen max-w-xs flex flex-col">
             <form className="bg-white shadow-md rounded px-8 py-6 mb-4 flex flex-col gap-4" onSubmit={handleSubmit}>
-                <input type="text" name="firstName" className="form-control border border-gray-300 p-2 rounded-md focus:outline-none shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" placeholder="First Name" defaultValue={formData.firstname} onChange={handleChange}/> 
-                <input type="text" name="lastName" className="form-control border border-gray-300 p-2 rounded-md focus:outline-none shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" placeholder="Last Name" defaultValue={formData.lastname} onChange={handleChange}/> 
+                <input type="text" name="firstname" className="form-control border border-gray-300 p-2 rounded-md focus:outline-none shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" placeholder="First Name" defaultValue={formData.firstname} onChange={handleChange}/> 
+                <input type="text" name="lastname" className="form-control border border-gray-300 p-2 rounded-md focus:outline-none shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" placeholder="Last Name" defaultValue={formData.lastname} onChange={handleChange}/> 
                 <input type="text" name="email" className="form-control border border-gray-300 p-2 rounded-md focus:outline-none shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" placeholder="Email" defaultValue={formData.email} onChange={handleChange}/> 
                 <div className="relative inline-block text-left">
                     <label htmlFor="status" className="block text-sm font-medium text-gray-700">Status</label>
--- server/database.sql
+++ server/database.sql
@@ -2,8 +2,8 @@ CREATE DATABASE axiomclone;
 
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     inPayroll BOOLEAN NOT NULL DEFAULT TRUE, 
